2de2a40615d13a80154bbb50a9ceb6e6400b6176
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-05-25 16:27:53 +0700

    16.27

--- src/App.js
+++ src/App.js
@@ -7,10 +7,14 @@ class App extends Component {
     console.log('constructor')
     console.log('---------------')
     this.state ={
-      favoritecolor:'green'
+      favoritecolor:'green',
+      show: true
     }
   }
 
+  delHeader = () =>{
+    this.setState({show: false}) 
+  }
   // static getDerivedStateFromProps(props,state){
   //   console.log('getDerivedStateFromProps')
   //   console.log(props,state)
@@ -28,14 +32,62 @@ class App extends Component {
     },3000);
   }
 
+  // changeColor = () => {
+  //   console.log('changeColor triggered')
+  //   console.log('---------------')
+  //   this.setState({favoritecolor:'orange'})
+  // }
+
+  // shouldComponentUpdate(){
+  //   return true;
+  // }
+
+  getSnapshotBeforeUpdate(prevProps,prevState){
+    console.log('getSnapshotBeforeUpdate')
+    console.log('---------------',prevProps)
+    return document.getElementById('div1').innerHTML =
+    "Before the update, the favorite color was " + prevState.favoritecolor;
+  }
+  
+  componentDidUpdate(){
+    console.log('componentDidUpdate')
+    console.log('---------------')
+    return document.getElementById('div2').innerHTML =
+    "The updated favorite color is " + this.state.favoritecolor;
+    
+  }
+
   render(){
     console.log('render')
     console.log('---------------')
+
+    let myHeader;
+    if(this.state.show){
+      myHeader = <Header />;
+    }
+
     return (
     <>
+      {myHeader}
+      <button onClick={this.delHeader}>Delete Header</button>
       <h1>My favorite color is {this.state.favoritecolor}</h1>
+      {/* <button onClick={this.changeColor}>Change color</button> */}
+      <div id="div1"></div>
+      <div id="div2"></div>
     </>
     );
+  }
+}
+
+class Header extends Component{
+  componentWillUnmount(){
+    alert('the header component is about to unmounted.');
+  }
+
+  render(){
+    return(
+      <h1>This is Header</h1>
+    )
   }
 }
 
